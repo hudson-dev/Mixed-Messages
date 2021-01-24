f7683ec2a971b1babf60e4034f73b3a3ceb7e2f7
hudson-dev <hudson.n.kim@gmail.com>
2021-01-24 00:05:11 -0800

    Finished Project!

--- README.md
+++ README.md
@@ -27,10 +27,10 @@ To run this project, git clone the repo and run with node.js.
 ## Features
 List of features ready and TODOs for future development
 * ~~Set Up Version Control~~
-* Store Mesage Components
-* Create the Message
-* Put the Program Together
-* Next Steps
+* ~~Store Mesage Components~~
+* ~~Create the Message~~
+* ~~Put the Program Together~~
+* ~~Next Steps~~
 
 ## Status
 Project is: _in progress_
--- main.js
+++ main.js
@@ -1,3 +1,18 @@
-const generateMessage = () => {
+const component1 = ["You shouldn't", "You should", "Defintely don't"];
+const component2 = ["go", "try", "attack"];
+const component3 = ["it.", "something new.", "seek discomfort."];
+const emojis = ["😂", "👋🏻", "❤️", "😀"];
 
+const components = [component1, component2, component3, emojis];
+
+const generateMessage = () => {
+    var message = [];
+    for (var component of components) {
+        const randomInt = Math.floor(Math.random() * component.length);
+        message.push(component[randomInt]);
+    }
+
+    console.log(message.join(" "));
 }
+
+generateMessage();
